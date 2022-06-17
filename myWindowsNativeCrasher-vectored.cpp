cc21947d328ee97a4866b40731b1ff33b65bfe3e
Bobby <bobbyg603@gmail.com>
2022-06-17 16:36:16 +0000

    Renamed to MyWindowsNativeCrasher

--- myWindowsNativeCrasher-vectored.cpp
+++ myWindowsNativeCrasher-vectored.cpp
@@ -3,7 +3,7 @@
 //
 //        The shared sample database 'Fred' is used in this example.
 //        You may view crashes for the Fred account by logging in at https://www.bugsplat.com:
-//        Account (Email Address): Fred@bugsplat.com
+//        Account (Email Address): Fred 
 //                       Password: Flintstone
 //
 //        In order to assure that crashes sent to the BugSplat website yield exception stack traces with file/line # information, 
@@ -34,9 +34,15 @@ void InvalidParameters();
 void OutOfBoundsVectorCrash();
 void VirtualFunctionCallCrash();
 void CustomSEHException();
-void CreateReport();
 bool ExceptionCallback(UINT nCode, LPVOID lpVal1, LPVOID lpVal2);
 MiniDmpSender *mpSender;
+
+LONG WINAPI VectoredExceptionHandler(PEXCEPTION_POINTERS pExp) {
+	if (pExp->ExceptionRecord->ExceptionCode == STATUS_HEAP_CORRUPTION) {
+		mpSender->createReport(pExp);
+		TerminateProcess(GetCurrentProcess(), pExp->ExceptionRecord->ExceptionCode);
+	} return EXCEPTION_CONTINUE_SEARCH;
+}
 
 int wmain(int argc, wchar_t **argv)
 {
@@ -47,14 +53,16 @@ int wmain(int argc, wchar_t **argv)
 		exit(0);
 	}
 
-	// BugSplat initialization.  Post crash reports to the "Fred" database for application "myConsoleCrasher" version "1.0"
-	mpSender = new MiniDmpSender(L"Fred", L"myConsoleCrasher", L"1.0", NULL, MDSF_USEGUARDMEMORY | MDSF_LOGFILE | MDSF_PREVENTHIJACKING);
+	// BugSplat initialization.  Post crash reports to the "Fred" database for application "MyWindowsNativeCrasher" version "1.0"
+	mpSender = new MiniDmpSender(L"Fred", L"MyWindowsNativeCrasher", L"1.0", NULL, MDSF_USEGUARDMEMORY | MDSF_LOGFILE | MDSF_PREVENTHIJACKING);
 
 	// The following calls add support for collecting crashes for abort(), vectored exceptions, out of memory,
 	// pure virtual function calls, and for invalid parameters for OS functions.
 	// These calls should be used for each module that links with a separate copy of the CRT.
 	SetGlobalCRTExceptionBehavior();
 	SetPerThreadCRTExceptionBehavior();  // This call needed in each thread of your app
+
+	//AddVectoredExceptionHandler(1, VectoredExceptionHandler); 
 
 	// A guard buffer of 20mb is needed to catch OutOfMemory crashes
 	mpSender->setGuardByteBufferSize(20*1024*1024);
@@ -63,9 +71,6 @@ int wmain(int argc, wchar_t **argv)
 	mpSender->setDefaultUserName(_T("Fred"));
 	mpSender->setDefaultUserEmail(_T("fred@bedrock.com"));
 	mpSender->setDefaultUserDescription(_T("This is the default user crash description."));
-
-	// Set optional notes field
-	mpSender->setNotes(_T("Additional 'notes' data supplied through API"));
 
 	// Process command line args that we need prior to crashing
 	for (int i = 1; i < argc; i++) {
@@ -131,12 +136,6 @@ int wmain(int argc, wchar_t **argv)
 			wprintf(L"Application normal exit.\n");
 			exit(0);
 		}
-
-		else if (!_wcsicmp(argv[i], L"/CreateReport")) {
-			CreateReport();
-			wprintf(L"Application normal exit.\n");
-			exit(0);
-		}
 	}
 
 	// Default if no crash resulted from command line args
@@ -150,7 +149,15 @@ void MemoryException()
 {
 	// Dereferencing a null pointer results in a memory exception
 	wprintf(L"MemoryException!\n");
-	*(volatile int *)0 = 0;
+	//*(volatile int *)0 = 0;
+
+	char* c = static_cast<char*>(malloc(10)); // allocate
+	wprintf(L"DivideByZero!\n");
+
+	memset(c - 1, 0, 10); // underwrite to stomp the CRT structure
+	wprintf(L"DivideByZero 2\n");
+	free(c); // this may produce Critical error detected c0000374
+	wprintf(L"DivideByZero 3\n");
 }
 
 void DivideByZero()
@@ -293,97 +300,6 @@ void CustomSEHException()
 	{
 		return;
 	}
-}
-
-void CreateReport()
-{
-	const __wchar_t  *xml = L"<report><process>"
-		"<exception>"
-			"<code>FATAL ERROR</code>"
-			"<explanation>This is an error code explanation</explanation>"
-			"<func><![CDATA[myConsoleCrasher!MemoryException]]></func>"
-			"<file>/www/bugsplatAutomation/myConsoleCrasher/myConsoleCrasher.cpp</file>"
-			"<line>143</line>"
-			"<registers>"
-				"<cs>0023</cs>"
-				"<ds>002b</ds>"
-				"<eax>00000011</eax>"
-				"<ebp>00affb58</ebp>"
-				"<ebx>00858000</ebx>"
-				"<ecx>43bf1e0e</ecx>"
-				"<edi>00affb58</edi>"
-				"<edx>014480b4</edx>"
-				"<efl>00010202</efl>"
-			"</registers>"
-		"</exception>"
-		"<modules numloaded=\"2\">"
-		"<module>"
-			"<name>myConsoleCrasher</name>"
-			"<order>1</order>"
-			"<address>01320000-01457000</address>"
-			"<path>C:/www/BugsplatAutomation/BugsplatAutomation/bin/x64/Release/temp/BugSplat/bin/myConsoleCrasher.exe</path>"
-			"<symbolsloaded>deferred</symbolsloaded>"
-			"<fileversion/>"
-			"<productversion/>"
-			"<checksum>00000000</checksum>"
-			"<timedatestamp>SatJun1501:18:092019</timedatestamp>"
-		"</module>"
-		"<module>"
-		"<name>BugSplatRc</name>"
-		"<order>2</order>"
-		"<address>01320000-01457000</address>"
-		"<path>C:/www/BugsplatAutomation/BugsplatAutomation/bin/x64/Release/BugSplatRc.dll</path>"
-		"<symbolsloaded>deferred</symbolsloaded>"
-		"<fileversion/>"
-		"<productversion/>"
-		"<checksum>00000000</checksum>"
-		"<timedatestamp>SatJun1501:18:092019</timedatestamp>"
-		"</module>"
-		"</modules>"
-		"<threads count=\"2\">"
-		"<thread id=\"0\" current=\"yes\" event=\"yes\" framecount=\"3\">"
-			"<frame>"
-				"<symbol><![CDATA[myConsoleCrasher!MemoryException]]></symbol>"
-				"<file>/www/bugsplatAutomation/myConsoleCrasher/myConsoleCrasher.cpp</file>"
-				"<line>143</line>"
-				"<offset>0x35</offset>"
-			"</frame>"
-			"<frame>"
-				"<symbol><![CDATA[myConsoleCrasher!wmain]]></symbol>"
-				"<file>C:/www/BugsplatAutomation/BugsplatAutomation/BugSplat/samples/myConsoleCrasher/myConsoleCrasher.cpp</file>"
-				"<line>83</line>"
-				"<offset>0x239</offset>"
-			"</frame>"
-			"<frame>"
-				"<symbol><![CDATA[myConsoleCrasher!__scrt_wide_environment_policy::initialize_environment]]></symbol>"
-				"<file>d:/agent/_work/4/s/src/vctools/crt/vcstartup/src/startup/exe_common.inl</file>"
-				"<line>90</line>"
-				"<offset>0x43</offset>"
-			"</frame>"
-		"</thread>"
-		"<thread id=\"1\" current=\"no\" event=\"no\" framecount=\"3\">"
-			"<frame>"
-				"<symbol><![CDATA[my2ConsoleCrasher!MemoryException]]></symbol>"
-				"<file>/www/bugsplatAutomation/myConsoleCrasher/myConsoleCrasher.cpp</file>"
-				"<line>143</line>"
-				"<offset>0x35</offset>"
-			"</frame>"
-			"<frame>"
-				"<symbol><![CDATA[my2ConsoleCrasher!wmain]]></symbol>"
-				"<file>C:/www/BugsplatAutomation/BugsplatAutomation/BugSplat/samples/myConsoleCrasher/myConsoleCrasher.cpp</file>"
-				"<line>83</line>"
-				"<offset>0x239</offset>"
-			"</frame>"
-			"<frame>"
-				"<symbol><![CDATA[my2ConsoleCrasher!__scrt_wide_environment_policy::initialize_environment]]></symbol>"
-				"<file>d:/agent/_work/4/s/src/vctools/crt/vcstartup/src/startup/exe_common.inl</file>"
-				"<line>90</line>"
-				"<offset>0x43</offset>"
-			"</frame>"
-		"</thread>"
-		"</threads></process></report>";
-
-	mpSender->createReport(xml);
 }
 
 
--- resource.h
+++ resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by myConsoleCrasher.rc
+// Used by MyWindowsNativeCrasher.rc
 
 // Next default values for new objects
 // 
